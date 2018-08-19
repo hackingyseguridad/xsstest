7a840d9c9141ab3a59126b1a8d97364f61b48f97
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-08-19 23:29:11 +0200

    Update xsstest.sh

--- xsstest.sh
+++ xsstest.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-Negro='\033[0;30m'
-Rojo='\033[0;31m'
-Verde='\033[0;32m'
-Amarillo='\033[0;33m'
-Azul='\033[0;34m'
-Purpura='\033[0;35m'
-Cyan='\033[0;36m'
-Blanco='\033[0;37m'
-Normal='\033[0m'
+Normal=$(echo 'printf' '\033[39m')
+Negro=$(echo 'printf' '\033[30m')
+Azul=$(echo 'printf' '\033[34m')
+Cyan=$(echo 'printf' '\033[36m')
+Gris=$(echo 'printf' '\033[90m')
+Verde=$(echo 'printf' '\033[32m')
+Magenta=$(echo 'printf' '\033[35m')
+Rojo=$(echo 'printf' '\033[31m')
+Blanco=$(echo 'printf' '\033[0m')
+BlancoAlt=$(echo 'printf' '\033[97m')
+Amarillo=$(echo 'printf' '\033[33m')
 
+$Verde
 cat << "INFO"
- __   __ _____ _____ _            _
- \ \ / // ____/ ____| |          | |
-  \ V /| (___| (___ | |_ ___  ___| |_
-   > <  \___ \\___ \| __/ _ \/ __| __|
-  / . \ ____) |___) | ||  __/\__ \ |_
- /_/ \_\_____/_____/ \__\___||___/\__|
-                     hackingyseguridad.con
+    __   __ _____ _____ _            _
+    \ \ / // ____/ ____| |          | |
+     \ V /| (___| (___ | |_ ___  ___| |_
+      > <  \___ \\___ \| __/ _ \/ __| __|
+     / . \ ____) |___) | ||  __/\__ \ |_
+    /_/ \_\_____/_____/ \__\___||___/\__| Beta
 INFO
-
+$BlancoAlt
+echo "                  hackingyseguridad.com"
+echo
+$Blanco
 if [ -z "$1" ]; then
-        printf "${Amarillo}" ; echo
         echo "Test vulnerabilidad XSS url web."
-        echo "Uso: $0 <URL>"; printf "${Normal}\n"
+        echo "Uso: $0 <URL>"
         exit 0
 fi
 
 echo
 echo "==========================================="
-echo "test sobre la url.: $1"
+echo "test xss sobre la url.: $1"
 echo "==========================================="
 echo
-
+$BlancoAlt
 for n in `cat diccionario.txt`
 do
         fqdn=$1"/"$n
@@ -40,3 +44,4 @@ do
         then echo $fqdn
         fi
 done
+$Normal
